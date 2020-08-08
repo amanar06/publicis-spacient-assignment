07fb9ac333ef6c17ffb09238ca61dc31d609bdfd
Aman Arora <arora.aman2013@gmail.com>
2020-08-08 23:11:00 +0530

    spacex mission layout

--- src/Components/launchPrograms/filter.js
+++ src/Components/launchPrograms/filter.js
@@ -14,50 +14,50 @@ function filter() {
                         <FormFeedback>Please Enter a valid year</FormFeedback>
                     </Form>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2006</Button></Col>
-                        <Col sm={6}><Button color="success">2007</Button></Col>
+                        <Col xs={6}><Button color="success">2006</Button></Col>
+                        <Col xs={6}><Button color="success">2007</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2007</Button></Col>
-                        <Col sm={6}><Button color="success">2008</Button></Col>
+                        <Col xs={6}><Button color="success">2007</Button></Col>
+                        <Col xs={6}><Button color="success">2008</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2009</Button></Col>
-                        <Col sm={6}><Button color="success">2010</Button></Col>
+                        <Col xs={6}><Button color="success">2009</Button></Col>
+                        <Col xs={6}><Button color="success">2010</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2011</Button></Col>
-                        <Col sm={6}><Button color="success">2012</Button></Col>
+                        <Col xs={6}><Button color="success">2011</Button></Col>
+                        <Col xs={6}><Button color="success">2012</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2013</Button></Col>
-                        <Col sm={6}><Button color="success">2014</Button></Col>
+                        <Col xs={6}><Button color="success">2013</Button></Col>
+                        <Col xs={6}><Button color="success">2014</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2015</Button></Col>
-                        <Col sm={6}><Button color="success">2016</Button></Col>
+                        <Col xs={6}><Button color="success">2015</Button></Col>
+                        <Col xs={6}><Button color="success">2016</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2017</Button></Col>
-                        <Col sm={6}><Button color="success">2018</Button></Col>
+                        <Col xs={6}><Button color="success">2017</Button></Col>
+                        <Col xs={6}><Button color="success">2018</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">2019</Button></Col>
-                        <Col sm={6}><Button color="success">2020</Button></Col>
+                        <Col xs={6}><Button color="success">2019</Button></Col>
+                        <Col xs={6}><Button color="success">2020</Button></Col>
                     </Row>
-                    <CardSubtitle className="pt-1 mb-2" style={{borderBottom: "2px solid #f4f4f4"}} >
+                    <CardSubtitle className="pt-3 mb-2" style={{borderBottom: "2px solid #f4f4f4"}} >
                         Successful Launch
                     </CardSubtitle>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">True</Button></Col>
-                        <Col sm={6}><Button color="success">False</Button></Col>
+                        <Col xs={6}><Button color="success">True</Button></Col>
+                        <Col xs={6}><Button color="success">False</Button></Col>
                     </Row>
-                    <CardSubtitle className="pt-1 mb-2" style={{borderBottom: "2px solid #f4f4f4"}}>
+                    <CardSubtitle className="pt-3 mb-2" style={{borderBottom: "2px solid #f4f4f4"}}>
                         Successful Landing
                     </CardSubtitle>
                     <Row className="pb-1">
-                        <Col sm={6}><Button color="success">True</Button></Col>
-                        <Col sm={6}><Button color="success">False</Button></Col>
+                        <Col xs={6}><Button color="success">True</Button></Col>
+                        <Col xs={6}><Button color="success">False</Button></Col>
                     </Row>
                 </CardBody>
             </Card>
--- src/Components/launchPrograms/launchPrograms.js
+++ src/Components/launchPrograms/launchPrograms.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { Container, Row, Col, Card } from "reactstrap";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Card, CardImg, CardTitle,  } from "reactstrap";
 import Filter from './filter'
+import axios from 'axios'
 
-function launchPrograms() {
+function LaunchPrograms() {
+  const [spacexData, setSpacexData] = useState([])
+  useEffect(()=>{
+    axios(`https://api.spacexdata.com/v3/launches?limit=20`).then(res => {
+        console.log(res.data)
+        setSpacexData(res.data)
+    })
+  },[])
   return (
     <div>
         <h3 className="p-2" style={{float: "left"}}>SpacEx Launch Programs</h3>
@@ -10,9 +18,34 @@ function launchPrograms() {
             <Col lg={3}>
                 <Filter />
             </Col>
+            <Col lg={9}>
+                <Row className="p-2">
+                    {spacexData.length>0 && spacexData.map((data, i) => (
+                        <Col lg={3} md={6} className="mb-2 pb-2" key={i}>
+                            <Card>
+                                <div className="m-2">
+                                <CardImg top width="100%" style={{backgroundColor: "#f4f4f4"}} src={data.links.mission_patch_small} alt="Card image cap" />
+                                </div>
+                                <CardTitle style={{color: "rgb(65, 83, 146)"}}>
+                                    {`${data.mission_name} #${data.flight_number}`}
+                                </CardTitle>
+                                <div className={`mx-2 d-flex justify-content-start ${data.mission_id.length>0?null:'pb-3'}`}><b>Mission Ids:</b></div>
+                                <li className="mx-2 d-flex justify-content-start">
+                                    {data.mission_id.map((item, i) => (
+                                        <ul key={i}>{item}</ul>    
+                                    ))}
+                                </li>
+                                <p className="mx-2 d-flex"><b>Launch Year: {data.launch_year}</b></p>
+                                <p className="mx-2 d-flex"><b>Successful Launch: {data.launch_success.toString()}</b></p>
+                                <p className="mx-2 d-flex"><b>Successful Landing: {data.rocket.first_stage.cores[0].land_success === null? '': data.rocket.first_stage.cores[0].land_success.toString()}</b></p>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Col>
         </Row>
     </div>
   );
 }
 
-export default launchPrograms;
+export default LaunchPrograms;
