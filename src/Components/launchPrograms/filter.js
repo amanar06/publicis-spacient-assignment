8f97ca67f96c326da4603cddc8fd7780c3ca6d34
Aman Arora <arora.aman2013@gmail.com>
2020-08-09 16:56:03 +0530

    enabled filters actions

--- src/Components/launchPrograms/filter.js
+++ src/Components/launchPrograms/filter.js
@@ -1,63 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, CardBody, CardTitle, CardSubtitle, Button, Row, Col, Input, Form, FormFeedback } from 'reactstrap'
 
-function filter() {
+function Filter({
+    year,
+    changeYear,
+    launchStatus,
+    landStatus,
+    changeLaunchStatus,
+    changeLandStatus,
+    ...props
+}) {
+    const [inputYear, setInputYear] = useState('')
+    const [invalid, setInvalid] = useState(false)
+    const submitYear = (e) => {
+        e.preventDefault()
+        if((inputYear>2005 && inputYear<2021) || inputYear === '' ){
+            setInvalid(false)
+            changeYear(inputYear)
+        }
+        else{
+            setInvalid(true)
+        }
+    }
     return (
         <div className="p-2">
             <Card>
                 <CardBody>
                     <CardTitle className="d-flex justify-content-start">
-                        Filters
+                        FilteFs
                     </CardTitle>
-                    <Form className="pb-2">
-                        <Input type="text" placeholder="Enter Year" style={{borderWidth: "0 0 2px", borderColor: "#f4f4f4", boxShadow: "none" }} />
+                    <Form className="pb-2" onSubmit={(e)=>submitYear(e)}>
+                        <Input type="text" placeholder="Launch Year" style={{borderWidth: "0 0 2px", borderColor: "#f4f4f4", boxShadow: "none" }} onChange={(e)=> setInputYear(e.target.value)} invalid={invalid}  />
                         <FormFeedback>Please Enter a valid year</FormFeedback>
                     </Form>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2006</Button></Col>
-                        <Col xs={6}><Button color="success">2007</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2006?'opacity-4':''}`} onClick={()=> year !== 2006?changeYear(2006):changeYear('')}>2006</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2007?'opacity-4':''}`} onClick={()=> year !== 2007?changeYear(2007):changeYear('')}>2007</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2007</Button></Col>
-                        <Col xs={6}><Button color="success">2008</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2008?'opacity-4':''}`} onClick={()=> year !== 2008?changeYear(2008):changeYear('')}>2008</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2009?'opacity-4':''}`} onClick={()=> year !== 2009?changeYear(2009):changeYear('')}>2009</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2009</Button></Col>
-                        <Col xs={6}><Button color="success">2010</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2010?'opacity-4':''}`} onClick={()=> year !== 2010?changeYear(2010):changeYear('')}>2010</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2011?'opacity-4':''}`} onClick={()=> year !== 2011?changeYear(2011):changeYear('')}>2011</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2011</Button></Col>
-                        <Col xs={6}><Button color="success">2012</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2012?'opacity-4':''}`} onClick={()=> year !== 2012?changeYear(2012):changeYear('')}>2012</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2013?'opacity-4':''}`} onClick={()=> year !== 2013?changeYear(2013):changeYear('')}>2013</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2013</Button></Col>
-                        <Col xs={6}><Button color="success">2014</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2014?'opacity-4':''}`} onClick={()=> year !== 2014?changeYear(2014):changeYear('')}>2014</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2015?'opacity-4':''}`} onClick={()=> year !== 2015?changeYear(2015):changeYear('')}>2015</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2015</Button></Col>
-                        <Col xs={6}><Button color="success">2016</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2016?'opacity-4':''}`} onClick={()=> year !== 2016?changeYear(2016):changeYear('')}>2016</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2017?'opacity-4':''}`} onClick={()=> year !== 2017?changeYear(2017):changeYear('')}>2017</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2017</Button></Col>
-                        <Col xs={6}><Button color="success">2018</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2018?'opacity-4':''}`} onClick={()=> year !== 2018?changeYear(2018):changeYear('')}>2018</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2019?'opacity-4':''}`} onClick={()=> year !== 2019?changeYear(2019):changeYear('')}>2019</Button></Col>
                     </Row>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">2019</Button></Col>
-                        <Col xs={6}><Button color="success">2020</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${year !== 2020?'opacity-4':''}`} onClick={()=> year !== 2020?changeYear(2020):changeYear('')}>2020</Button></Col>
                     </Row>
                     <CardSubtitle className="pt-3 mb-2" style={{borderBottom: "2px solid #f4f4f4"}} >
                         Successful Launch
                     </CardSubtitle>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">True</Button></Col>
-                        <Col xs={6}><Button color="success">False</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${launchStatus !== "true"?'opacity-4':''}`} onClick={()=>launchStatus !== 'true'?changeLaunchStatus('true'):changeLaunchStatus('')}>True</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${launchStatus !== "false"?'opacity-4':''}`} onClick={()=>launchStatus !== 'false'?changeLaunchStatus('false'):changeLaunchStatus('')}>False</Button></Col>
                     </Row>
                     <CardSubtitle className="pt-3 mb-2" style={{borderBottom: "2px solid #f4f4f4"}}>
                         Successful Landing
                     </CardSubtitle>
                     <Row className="pb-1">
-                        <Col xs={6}><Button color="success">True</Button></Col>
-                        <Col xs={6}><Button color="success">False</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${landStatus !== "true"?'opacity-4':''}`} onClick={()=>landStatus !== 'true' ? changeLandStatus('true'):changeLandStatus('')}>True</Button></Col>
+                        <Col xs={6}><Button color="success" className={`shadow-none ${landStatus !== "false"?'opacity-4':''}`} onClick={()=>landStatus !== "false"?changeLandStatus('false'):changeLandStatus('')}>False</Button></Col>
                     </Row>
                 </CardBody>
             </Card>
@@ -65,4 +84,4 @@ function filter() {
     )
 }
 
-export default filter
+export default Filter
--- src/Components/launchPrograms/launchPrograms.js
+++ src/Components/launchPrograms/launchPrograms.js
@@ -5,18 +5,22 @@ import axios from 'axios'
 
 function LaunchPrograms() {
   const [spacexData, setSpacexData] = useState([])
+  const [year, setYear] = useState('')
+  const [launchStatus, setLaunchStatus] = useState('')
+  const [landStatus, setLandStatus] = useState('')
   useEffect(()=>{
-    axios(`https://api.spacexdata.com/v3/launches?limit=20`).then(res => {
+    axios(`https://api.spacexdata.com/v3/launches?limit=100&launch_success=${launchStatus}&land_success=${landStatus}&launch_year=${year}`).then(res => {
         console.log(res.data)
         setSpacexData(res.data)
     })
-  },[])
+  },[year, launchStatus, landStatus])
+
   return (
-    <div>
+    <div style={{maxWidth: "1440px", margin: "0 auto"}}>
         <h3 className="p-2" style={{float: "left"}}>SpacEx Launch Programs</h3>
         <Row style={{width: "100%"}}>
             <Col lg={3}>
-                <Filter />
+                <Filter year={year} changeYear={setYear} launchStatus={launchStatus} landStatus={landStatus} changeLaunchStatus={setLaunchStatus} changeLandStatus={setLandStatus} />
             </Col>
             <Col lg={9}>
                 <Row className="p-2">
@@ -24,9 +28,9 @@ function LaunchPrograms() {
                         <Col lg={3} md={6} className="mb-2 pb-2" key={i}>
                             <Card>
                                 <div className="m-2">
-                                <CardImg top width="100%" style={{backgroundColor: "#f4f4f4"}} src={data.links.mission_patch_small} alt="Card image cap" />
+                                <CardImg top width="100%" style={{backgroundColor: "#f4f4f4"}} src={data.links.mission_patch_small} alt={`${data.rocket.rocket_name} img`} />
                                 </div>
-                                <CardTitle style={{color: "rgb(65, 83, 146)"}}>
+                                <CardTitle style={{color: "rgb(65, 83, 146)"}} className="px-2">
                                     {`${data.mission_name} #${data.flight_number}`}
                                 </CardTitle>
                                 <div className={`mx-2 d-flex justify-content-start ${data.mission_id.length>0?null:'pb-3'}`}><b>Mission Ids:</b></div>
@@ -36,7 +40,7 @@ function LaunchPrograms() {
                                     ))}
                                 </li>
                                 <p className="mx-2 d-flex"><b>Launch Year: {data.launch_year}</b></p>
-                                <p className="mx-2 d-flex"><b>Successful Launch: {data.launch_success.toString()}</b></p>
+                                <p className="mx-2 d-flex"><b>Successful Launch: {data.launch_success === null?'':data.launch_success.toString()}</b></p>
                                 <p className="mx-2 d-flex"><b>Successful Landing: {data.rocket.first_stage.cores[0].land_success === null? '': data.rocket.first_stage.cores[0].land_success.toString()}</b></p>
                             </Card>
                         </Col>
