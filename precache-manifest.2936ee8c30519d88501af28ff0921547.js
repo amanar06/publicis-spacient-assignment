dd8aad22c049aff7b380a507e0bccc95b5f2465f
Aman Arora <arora.aman2013@gmail.com>
2020-08-09 17:04:06 +0530

    Updates

---
+++ precache-manifest.2936ee8c30519d88501af28ff0921547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541df1ecf5bfda6a44cee6b497f426ab",
+    "url": "/publicis-sapient/index.html"
+  },
+  {
+    "revision": "d6800860cfb5ec87028b",
+    "url": "/publicis-sapient/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "4e5b30b93c2e80376d57",
+    "url": "/publicis-sapient/static/css/main.b8db655d.chunk.css"
+  },
+  {
+    "revision": "d6800860cfb5ec87028b",
+    "url": "/publicis-sapient/static/js/2.c0785b38.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/publicis-sapient/static/js/2.c0785b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5b30b93c2e80376d57",
+    "url": "/publicis-sapient/static/js/main.c7bb32ab.chunk.js"
+  },
+  {
+    "revision": "3a5b61f7d1a505c60b2e",
+    "url": "/publicis-sapient/static/js/runtime-main.a9221028.js"
+  }
+]);
